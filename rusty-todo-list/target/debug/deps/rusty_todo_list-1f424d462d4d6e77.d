65e988b337b0fe86d3cba8cc8527eee044aed08a
Andres Abreu <andresabreu@andress-MacBook-Pro.local>
2022-09-07 16:30:27 -0400

    first run test

---
+++ rusty-todo-list/target/debug/deps/rusty_todo_list-1f424d462d4d6e77.d
@@ -0,0 +1,5 @@
+/Users/andresabreu/Desktop/rusty-todo-list/rusty-todo-list/target/debug/deps/rusty_todo_list-1f424d462d4d6e77: src/main.rs
+
+/Users/andresabreu/Desktop/rusty-todo-list/rusty-todo-list/target/debug/deps/rusty_todo_list-1f424d462d4d6e77.d: src/main.rs
+
+src/main.rs:
---
+++ rusty-todo-list/target/debug/rusty-todo-list.d
@@ -0,0 +1,1 @@
+/Users/andresabreu/Desktop/rusty-todo-list/rusty-todo-list/target/debug/rusty-todo-list: /Users/andresabreu/Desktop/rusty-todo-list/rusty-todo-list/src/main.rs
