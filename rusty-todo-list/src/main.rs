457e6464547065fb595af2cc4a49c1dfaeaa659d
Andres Abreu <andresabreu@andress-MacBook-Pro.local>
2022-09-07 17:22:23 -0400

    code mods in main.rs file

--- rusty-todo-list/src/main.rs
+++ rusty-todo-list/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+struct Todo {
+    // use rust biult in HashMap to store Key - val pairs
+    map: HashMap<String, bool>,
 }
+
