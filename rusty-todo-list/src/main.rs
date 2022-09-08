6aafc9ea3f10be965f62cdb8ceda772c67c4d2aa
Andres Abreu <andresabreu@andress-MacBook-Pro.local>
2022-09-07 20:07:46 -0400

    main.rs code additions

--- rusty-todo-list/src/main.rs
+++ rusty-todo-list/src/main.rs
@@ -1,7 +1,42 @@
 use std::collections::HashMap;
 
+fn main() {
+    let action = std::env::args().nth(1).expect("Please specify an action");
+    let item = std::env::args().nth(2).expect("Please specify an item");
+
+}
+
+fn main(){
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+        Ok(_) => println!("todo saved"),
+        Err(why) => println!("An error occurred: {}", why),
+        }
+    }
+}
+
 struct Todo {
     // use rust biult in HashMap to store Key - val pairs
     map: HashMap<String, bool>,
+}
+
+impl Todo {
+    fn insert(&mut self, key: String) {
+        // insert a new item into our map
+        // we pass true as value
+        self.map.insert(key, true);
+    }
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
+    }
 }
 
